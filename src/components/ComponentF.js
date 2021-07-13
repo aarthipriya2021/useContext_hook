e75022bf47dea98abe6b88add43a0e25955c772d
aarthipriya2021 <a.aarthipriya2021@gmail.com>
2021-07-13 11:03:47 +0530

    useContext hook

---
+++ src/components/ComponentF.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ChannelContext, UserContext } from '../App';
+
+function ComponentF() {
+    return (
+        <div>
+            <UserContext.Consumer>
+                {user => { return (
+                    <ChannelContext.Consumer>
+                        {channel => {
+                            return(<div>userValue = {user}, userGroup={channel}.</div>)
+                        }}
+                        
+                    </ChannelContext.Consumer>
+                )  
+                }}
+            </UserContext.Consumer>
+        </div>
+    )
+}
+
+export default ComponentF
